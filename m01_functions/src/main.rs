c193dedcba2d7bf0a1eec154392bfea5f2e84b56
fahad <fahadalisarwar@gmail.com>
2023-10-27 13:53:39 +0200

    added array example

--- m01_functions/src/main.rs
+++ m01_functions/src/main.rs
@@ -4,4 +4,8 @@ fn divide(n1: f64, n2: f64) -> f64{
 fn main() {
     let res = divide(2.4, 3.3);
     println!("{:.5}", res);
+
+    let pi: f64= 3.12;
+    let cosine: f64 = pi.cos();
+    println!("{:.5}", cosine);
 }
---
+++ m02_array_n_slices/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let arr: [i32; 3] = [10, 20, 40];
+    let first = arr[0];
+    println!("{}", first);
+    for i in 0..=2{
+        println!("{}", arr[i]);
+    }
+    println!("{}", arr.len());
+    // println!("{}", arr[5]);
+
+    // slices
+    
+}
