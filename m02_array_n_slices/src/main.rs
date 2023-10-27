b6aacbfdf62115a1978728f95d31ee0f55545c3c
fahad <fahadalisarwar@gmail.com>
2023-10-27 14:00:37 +0200

    added slices example

--- m02_array_n_slices/src/main.rs
+++ m02_array_n_slices/src/main.rs
@@ -1,5 +1,13 @@
+fn sum(values: &[i32])->i32{
+    let mut summation = 0;
+    for i in 0..values.len()    {
+        summation += values[i]
+    }
+    summation
+}
+
 fn main() {
-    let arr: [i32; 3] = [10, 20, 40];
+    let arr: [i32; 5] = [10, 244, 40, 77,88];
     let first = arr[0];
     println!("{}", first);
     for i in 0..=2{
@@ -9,5 +17,10 @@ fn main() {
     // println!("{}", arr[5]);
 
     // slices
-    
+    let answer = sum(&arr);
+    println!("{}", answer);
+
+    println!("{:?}", arr);
+    let arr_slice = &arr[0..2];
+    println!("{:?}", arr_slice);
 }
